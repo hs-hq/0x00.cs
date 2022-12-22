6294b132efd6f7e034291ffa1f021cfdfc3433c2
PapaMuziko <guillaume@holbertonschool.com>
2022-12-21 17:25:09 -0800

    first commit

---
+++ 10-copy_cut_paste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+		string word = "Holberton";
+		// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+		Console.WriteLine("First 3 letters: {0}", word_first_3);
+		Console.WriteLine("Last 2 letters: {0}", word_last_2);
+		Console.WriteLine("Middle word: {0}", middle_word);
+        }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# 0x00. C# - Hello World
+
+Source code examples for project 0x00
